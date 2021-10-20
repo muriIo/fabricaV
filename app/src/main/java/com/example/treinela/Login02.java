6fee3f905357a4c1a5e913e21f155242b1b23faf
joaoperanovich <joao_peranovich@hotmail.com>
2021-10-20 19:18:20 -0300

    Tela Login e HOME - JP

---
+++ app/src/main/java/com/example/treinela/Login02.java
@@ -0,0 +1,21 @@
+package com.example.treinela;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class Login02 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login02);
+    }
+
+    public void btnLogar(View view){
+        return;
+    }
+
+
+}
